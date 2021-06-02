877c653eb6ac33fb9322209752b784c42a01210b
Владислав Лавриненко <mitelvlad99sea@gmail.com>
2021-06-02 12:53:02 +0300

    Add project files.

---
+++ Lab6_3/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿
+namespace Lab6_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Y_Label = new System.Windows.Forms.Label();
+            this.X_Label = new System.Windows.Forms.Label();
+            this.Y_box = new System.Windows.Forms.TextBox();
+            this.X_box = new System.Windows.Forms.TextBox();
+            this.Color2_Button = new System.Windows.Forms.Button();
+            this.Color1_Button = new System.Windows.Forms.Button();
+            this.Color2_Label = new System.Windows.Forms.Label();
+            this.Color1_Label = new System.Windows.Forms.Label();
+            this.One_Label = new System.Windows.Forms.Label();
+            this.Zero_Label = new System.Windows.Forms.Label();
+            this.Label_Two_Left = new System.Windows.Forms.Label();
+            this.Label_Two_Right = new System.Windows.Forms.Label();
+            this.Label_Two_Up = new System.Windows.Forms.Label();
+            this.Label_Two_Down = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Play = new System.Windows.Forms.ToolStripMenuItem();
+            this.Pause = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Y_Label);
+            this.panel1.Controls.Add(this.X_Label);
+            this.panel1.Controls.Add(this.Y_box);
+            this.panel1.Controls.Add(this.X_box);
+            this.panel1.Controls.Add(this.Color2_Button);
+            this.panel1.Controls.Add(this.Color1_Button);
+            this.panel1.Controls.Add(this.Color2_Label);
+            this.panel1.Controls.Add(this.Color1_Label);
+            this.panel1.Controls.Add(this.One_Label);
+            this.panel1.Controls.Add(this.Zero_Label);
+            this.panel1.Controls.Add(this.Label_Two_Left);
+            this.panel1.Controls.Add(this.Label_Two_Right);
+            this.panel1.Controls.Add(this.Label_Two_Up);
+            this.panel1.Controls.Add(this.Label_Two_Down);
+            this.panel1.Location = new System.Drawing.Point(12, 34);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(760, 760);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // Y_Label
+            // 
+            this.Y_Label.AutoSize = true;
+            this.Y_Label.BackColor = System.Drawing.Color.Transparent;
+            this.Y_Label.Location = new System.Drawing.Point(390, 35);
+            this.Y_Label.Name = "Y_Label";
+            this.Y_Label.Size = new System.Drawing.Size(17, 20);
+            this.Y_Label.TabIndex = 2;
+            this.Y_Label.Text = "Y";
+            this.Y_Label.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Label_MouseUp);
+            // 
+            // X_Label
+            // 
+            this.X_Label.AutoSize = true;
+            this.X_Label.BackColor = System.Drawing.Color.Transparent;
+            this.X_Label.Location = new System.Drawing.Point(708, 339);
+            this.X_Label.Name = "X_Label";
+            this.X_Label.Size = new System.Drawing.Size(18, 20);
+            this.X_Label.TabIndex = 3;
+            this.X_Label.Text = "X";
+            this.X_Label.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Label_MouseUp);
+            // 
+            // Y_box
+            // 
+            this.Y_box.Enabled = false;
+            this.Y_box.Location = new System.Drawing.Point(708, 733);
+            this.Y_box.Name = "Y_box";
+            this.Y_box.Size = new System.Drawing.Size(50, 27);
+            this.Y_box.TabIndex = 14;
+            // 
+            // X_box
+            // 
+            this.X_box.Enabled = false;
+            this.X_box.Location = new System.Drawing.Point(708, 700);
+            this.X_box.Name = "X_box";
+            this.X_box.Size = new System.Drawing.Size(50, 27);
+            this.X_box.TabIndex = 13;
+            // 
+            // Color2_Button
+            // 
+            this.Color2_Button.Location = new System.Drawing.Point(185, 722);
+            this.Color2_Button.Name = "Color2_Button";
+            this.Color2_Button.Size = new System.Drawing.Size(48, 29);
+            this.Color2_Button.TabIndex = 12;
+            this.Color2_Button.Text = "...";
+            this.Color2_Button.UseVisualStyleBackColor = true;
+            this.Color2_Button.Click += new System.EventHandler(this.Color2_Button_Click);
+            // 
+            // Color1_Button
+            // 
+            this.Color1_Button.Location = new System.Drawing.Point(185, 690);
+            this.Color1_Button.Name = "Color1_Button";
+            this.Color1_Button.Size = new System.Drawing.Size(48, 29);
+            this.Color1_Button.TabIndex = 4;
+            this.Color1_Button.Text = "...";
+            this.Color1_Button.UseVisualStyleBackColor = true;
+            this.Color1_Button.Click += new System.EventHandler(this.Color1_Button_Click);
+            // 
+            // Color2_Label
+            // 
+            this.Color2_Label.AutoSize = true;
+            this.Color2_Label.Location = new System.Drawing.Point(9, 726);
+            this.Color2_Label.Name = "Color2_Label";
+            this.Color2_Label.Size = new System.Drawing.Size(170, 20);
+            this.Color2_Label.TabIndex = 11;
+            this.Color2_Label.Text = "Цвет большой области";
+            // 
+            // Color1_Label
+            // 
+            this.Color1_Label.AutoSize = true;
+            this.Color1_Label.Location = new System.Drawing.Point(9, 694);
+            this.Color1_Label.Name = "Color1_Label";
+            this.Color1_Label.Size = new System.Drawing.Size(151, 20);
+            this.Color1_Label.TabIndex = 10;
+            this.Color1_Label.Text = "Цвет малой области";
+            // 
+            // One_Label
+            // 
+            this.One_Label.AutoSize = true;
+            this.One_Label.BackColor = System.Drawing.Color.Transparent;
+            this.One_Label.Location = new System.Drawing.Point(358, 297);
+            this.One_Label.Name = "One_Label";
+            this.One_Label.Size = new System.Drawing.Size(17, 20);
+            this.One_Label.TabIndex = 9;
+            this.One_Label.Text = "1";
+            this.One_Label.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Label_MouseUp);
+            // 
+            // Zero_Label
+            // 
+            this.Zero_Label.AutoSize = true;
+            this.Zero_Label.BackColor = System.Drawing.Color.Transparent;
+            this.Zero_Label.Location = new System.Drawing.Point(358, 358);
+            this.Zero_Label.Name = "Zero_Label";
+            this.Zero_Label.Size = new System.Drawing.Size(17, 20);
+            this.Zero_Label.TabIndex = 4;
+            this.Zero_Label.Text = "0";
+            this.Zero_Label.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Label_MouseUp);
+            // 
+            // Label_Two_Left
+            // 
+            this.Label_Two_Left.AutoSize = true;
+            this.Label_Two_Left.BackColor = System.Drawing.Color.Transparent;
+            this.Label_Two_Left.Location = new System.Drawing.Point(508, 358);
+            this.Label_Two_Left.Name = "Label_Two_Left";
+            this.Label_Two_Left.Size = new System.Drawing.Size(17, 20);
+            this.Label_Two_Left.TabIndex = 8;
+            this.Label_Two_Left.Text = "2";
+            this.Label_Two_Left.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Label_MouseUp);
+            // 
+            // Label_Two_Right
+            // 
+            this.Label_Two_Right.AutoSize = true;
+            this.Label_Two_Right.BackColor = System.Drawing.Color.Transparent;
+            this.Label_Two_Right.Location = new System.Drawing.Point(226, 358);
+            this.Label_Two_Right.Name = "Label_Two_Right";
+            this.Label_Two_Right.Size = new System.Drawing.Size(17, 20);
+            this.Label_Two_Right.TabIndex = 7;
+            this.Label_Two_Right.Text = "2";
+            this.Label_Two_Right.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Label_MouseUp);
+            // 
+            // Label_Two_Up
+            // 
+            this.Label_Two_Up.AutoSize = true;
+            this.Label_Two_Up.BackColor = System.Drawing.Color.Transparent;
+            this.Label_Two_Up.Location = new System.Drawing.Point(358, 231);
+            this.Label_Two_Up.Name = "Label_Two_Up";
+            this.Label_Two_Up.Size = new System.Drawing.Size(17, 20);
+            this.Label_Two_Up.TabIndex = 5;
+            this.Label_Two_Up.Text = "2";
+            this.Label_Two_Up.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Label_MouseUp);
+            // 
+            // Label_Two_Down
+            // 
+            this.Label_Two_Down.AutoSize = true;
+            this.Label_Two_Down.BackColor = System.Drawing.Color.Transparent;
+            this.Label_Two_Down.Location = new System.Drawing.Point(386, 507);
+            this.Label_Two_Down.Name = "Label_Two_Down";
+            this.Label_Two_Down.Size = new System.Drawing.Size(17, 20);
+            this.Label_Two_Down.TabIndex = 6;
+            this.Label_Two_Down.Text = "2";
+            this.Label_Two_Down.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Label_MouseUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Play,
+            this.Pause});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(782, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Play
+            // 
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(50, 24);
+            this.Play.Text = "&Play";
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // Pause
+            // 
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(60, 24);
+            this.Pause.Text = "Pa&use";
+            this.Pause.Click += new System.EventHandler(this.Pause_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 803);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Play;
+        private System.Windows.Forms.ToolStripMenuItem Pause;
+        private System.Windows.Forms.Label Y_Label;
+        private System.Windows.Forms.Label X_Label;
+        private System.Windows.Forms.Label Zero_Label;
+        private System.Windows.Forms.Label Label_Two_Left;
+        private System.Windows.Forms.Label Label_Two_Right;
+        private System.Windows.Forms.Label Label_Two_Up;
+        private System.Windows.Forms.Label Label_Two_Down;
+        private System.Windows.Forms.Label One_Label;
+        private System.Windows.Forms.Button Color2_Button;
+        private System.Windows.Forms.Button Color1_Button;
+        private System.Windows.Forms.Label Color2_Label;
+        private System.Windows.Forms.Label Color1_Label;
+        private System.Windows.Forms.TextBox Y_box;
+        private System.Windows.Forms.TextBox X_box;
+
+        private class solo
+        {
+            public solo()
+            {
+            }
+        }
+    }
+}
+
---
+++ Lab6_3/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.Timers;
+
+// spaghetti code 
+// dodelat 3,4 punkti
+namespace Lab6_3
+{
+
+    public partial class Form1 : Form
+    {
+        private static Pen BlackPen = new Pen(Color.Black, 2);
+        private SolidBrush GreyBrush = new SolidBrush(Color.Gray);
+        private SolidBrush GreyBrush1 = new SolidBrush(Color.Gray);
+        private Pen RainbowPen = new Pen(Color.Black, 3);
+        private static Pen DottedPen = new Pen(Color.Black, 3);
+        private bool DiscoFlag = false;
+        private static int DLarge = 240, D = 120;
+        private static int RLarge = 120, R = 60;
+        private int FirstSegment, SecondSegment, ThirdSegment;
+        private Point[] NewLines;
+        private Point[] NewLines1;
+        private bool small = true, large = true;
+        System.Windows.Forms.Timer aTimer = new System.Windows.Forms.Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Color1_Button_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                GreyBrush.Color = dialog.Color;
+                panel1.Invalidate();
+            }
+
+        }
+        private void Color2_Button_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                GreyBrush1.Color = dialog.Color;
+                panel1.Invalidate();
+            }
+
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            AdjustableArrowCap bigArrow = new AdjustableArrowCap(10, 10);
+            BlackPen.CustomEndCap = bigArrow;
+            DottedPen.DashStyle = DashStyle.Dash;
+            Graphics Axis = e.Graphics;
+            Graphics Figures = e.Graphics;
+            NewLines = First(e);
+            NewLines1 = Second(e);
+
+            Figures.FillPolygon(GreyBrush, NewLines);
+            Figures.FillPolygon(GreyBrush1, NewLines1);
+
+            Axis.DrawLine(BlackPen, 30, panel1.Height / 2, panel1.Width - 30, panel1.Height / 2);//X axis
+            Axis.DrawLine(BlackPen, panel1.Width / 2, panel1.Height - 30, panel1.Width / 2, 30);//Y axis
+
+            Figures.DrawEllipse(RainbowPen, (panel1.Width - DLarge) / 2, (panel1.Height - DLarge) / 2, DLarge, DLarge);// large ellipse
+            Figures.DrawEllipse(RainbowPen, (panel1.Width - D) / 2, (panel1.Height - D) / 2, D, D);// small ellipse center
+            Figures.DrawEllipse(RainbowPen, (panel1.Width - D) / 2 + 60, (panel1.Height - D) / 2, D, D);// small ellipse right
+            Figures.DrawEllipse(BlackPen, 498, 378, 4, 4);//dot right
+            Figures.DrawLine(RainbowPen, 380 - 120, 380 + 60, 380 + 180, 380 - 90); // y = 2x 
+            Figures.DrawLine(DottedPen, 380, 380 - 60, 380 + 120, 380 - 60); //horisontal dash
+            Figures.DrawLine(DottedPen, 380 + 120, 380 - 60, 380 + 120, 380);//vertical dash
+        }
+        private void Play_Click(object sender, EventArgs e)
+        {
+            DiscoFlag = true;
+            Timer();
+        }
+        private void Timer()
+        {
+            aTimer.Interval = 1000;
+            aTimer.Enabled = true;
+            aTimer.Tick += new EventHandler(Redraw);
+        }
+        private void Label_MouseUp(object sender, MouseEventArgs e)
+        {
+            Label label = (sender as Label);
+            Point temp = new Point(label.Location.X + e.X - Convert.ToInt32(label.Font.Size), label.Location.Y + e.Y - Convert.ToInt32(label.Font.Size)); ;
+            label.Location = temp;
+        }
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Random random = new Random();
+            int min = 0, max = 255;
+            X_box.Text = Convert.ToString(e.X);
+            Y_box.Text = Convert.ToString(e.Y);
+            GraphicsPath SmallPath = new GraphicsPath();
+            GraphicsPath LargePath = new GraphicsPath();
+            SmallPath.AddPolygon(NewLines);
+            LargePath.AddPolygon(NewLines1);
+            Region SmallRegion = new Region(SmallPath);
+            Region LargeRegion = new Region(LargePath);
+            Point temp = new Point(e.X, e.Y);
+            if (SmallRegion.IsVisible(temp) && small)
+            {
+                small = false;
+                GreyBrush.Color = Color.FromArgb(random.Next(min, max), random.Next(min, max), random.Next(min, max));
+                panel1.Invalidate();
+            }
+            if (LargeRegion.IsVisible(temp) && large)
+            {
+                large = false;
+                GreyBrush1.Color = Color.FromArgb(random.Next(min, max), random.Next(min, max), random.Next(min, max));
+                panel1.Invalidate();
+            }
+            if (!SmallRegion.IsVisible(temp))
+            {
+                small = true;
+            }
+            if (!LargeRegion.IsVisible(temp))
+            {
+                large = true;
+            }
+        }
+
+        private void Redraw(Object myObject, EventArgs myEventArgs)
+        {
+            if (DiscoFlag)
+            {
+                Random random = new Random();
+                int min = 0, max = 255;
+                int f = random.Next(6, 25);
+                Label_Two_Down.Font = new Font("Segoe UI", f);
+                Label_Two_Left.Font = new Font("Segoe UI", f);
+                Label_Two_Right.Font = new Font("Segoe UI", f);
+                Label_Two_Up.Font = new Font("Segoe UI", f);
+                One_Label.Font = new Font("Segoe UI", f);
+                Zero_Label.Font = new Font("Segoe UI", f);
+                X_Label.Font = new Font("Segoe UI", f);
+                Y_Label.Font = new Font("Segoe UI", f);
+
+                GreyBrush.Color = Color.FromArgb(random.Next(min, max), random.Next(min, max), random.Next(min, max));
+                GreyBrush1.Color = Color.FromArgb(random.Next(min, max), random.Next(min, max), random.Next(min, max));
+                panel1.Invalidate();
+            }
+            else
+                aTimer.Stop();
+        }
+        private void Pause_Click(object sender, EventArgs e)
+        {
+            DiscoFlag = false;
+        }
+        private Point[] First(PaintEventArgs e)
+        {
+            FirstSegment = R + R - 8 * R / 5;
+            SecondSegment = R - Convert.ToInt32(Math.Ceiling(2 * R / Math.Sqrt(5))); //x
+
+            Point[] curvePoints = new Point[FirstSegment + 1 + SecondSegment + 1];
+            for (int i = 0; i + R - FirstSegment <= R; i++)
+            {
+                Point temp = new Point(380 + R * 2 - i, 380 - Convert.ToInt32(Math.Sqrt(R * R - (i - R) * (i - R))));
+                curvePoints[i] = temp;
+            }
+
+            for (int i = 0; i + R - SecondSegment <= R; i++)
+            {
+                Point temp = new Point(380 + i + R - SecondSegment, 380 - Convert.ToInt32(Math.Sqrt(R * R - (i + R - SecondSegment) * (i + R - SecondSegment))));
+                curvePoints[FirstSegment + 1 + i] = temp;
+            }
+            return curvePoints;
+        }
+        private Point[] Second(PaintEventArgs e)
+        {
+            FirstSegment = Convert.ToInt32(Math.Ceiling(2 * RLarge / Math.Sqrt(5)));
+            ThirdSegment = R / 2; //x^2 = (x-R)^2
+            SecondSegment = 8 * R / 5 - ThirdSegment;
+
+            Point[] curvePoints1 = new Point[FirstSegment + 1 + SecondSegment + 1 + ThirdSegment + 1];
+
+            for (int i = 0; i <= FirstSegment; i++)
+            {
+                Point temp = new Point(380 + i, 380 - Convert.ToInt32(Math.Sqrt(RLarge * RLarge - i * i)));
+                curvePoints1[i] = temp;
+            }
+
+            for (int i = 0; i <= SecondSegment; i++)
+            {
+                Point temp = new Point(380 + ThirdSegment + i, 380 - Convert.ToInt32(Math.Sqrt(R * R - (ThirdSegment + i - R) * (ThirdSegment + i - R))));
+                curvePoints1[FirstSegment + 1 + SecondSegment - i] = temp;
+            }
+
+            for (int i = 0; i <= ThirdSegment; i++)
+            {
+                Point temp = new Point(380 + i, 380 - Convert.ToInt32(Math.Sqrt(R * R - i * i)));
+                curvePoints1[FirstSegment + 1 + SecondSegment + 1 + ThirdSegment - i] = temp;
+            }
+            return curvePoints1;
+        }
+    }
+}
